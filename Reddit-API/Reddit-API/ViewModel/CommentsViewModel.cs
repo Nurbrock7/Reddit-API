90e092c16e7d853fac397a9ea4ffc47568023aa7
Moegamat-Nur Brock <Nurbrock7@gmail.com>
2022-02-02 15:11:04 +0200

    Added ViewModel

---
+++ Reddit-API/Reddit-API/ViewModel/CommentsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Reddit_API.Models;
+using System.Collections.Generic;
+
+namespace Reddit_API.ViewModel
+{
+    public class CommentsViewModel
+    {
+        public Post post { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
---
+++ Reddit-API/Reddit-API/ViewModel/PostViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Reddit_API.Models;
+using System.Collections.Generic;
+
+namespace Reddit_API.ViewModel
+{
+    public class PostViewModel
+    {
+        public Topic topic { get; set; }
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}
