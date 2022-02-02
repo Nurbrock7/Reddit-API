8d8c795c3421caf8ac56adf55321b7001c632d72
Moegamat-Nur Brock <Nurbrock7@gmail.com>
2022-02-02 21:02:08 +0200

    Added new View Models and Controllers for Login and Registeration

---
+++ Reddit-API/Reddit-API/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Reddit_API.Models;
+using Microsoft.AspNetCore.Identity;
+using Reddit_API.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace Reddit_API.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly DbContext _db;
+        private readonly UserManager<ApplicationsUser> _userManager;
+        private readonly SignInManager<ApplicationsUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationsUser> userManager, SignInManager<ApplicationsUser> signInManager, DbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Reddit-API/Reddit-API/ViewModel/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Reddit_API.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+    }
+}
