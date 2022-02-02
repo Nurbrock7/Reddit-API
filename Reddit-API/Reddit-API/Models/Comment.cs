a21655984378d7441e48b065f07a2afdee881379
Moegamat-Nur Brock <Nurbrock7@gmail.com>
2022-02-02 14:18:56 +0200

    Update : Added Comment model and removed EV Model

---
+++ Reddit-API/Reddit-API/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Reddit_API.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public string UserName { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+    }
+}
--- Reddit-API/Reddit-API/Models/Topic.cs
+++ Reddit-API/Reddit-API/Models/Topic.cs
@@ -6,7 +6,6 @@ namespace Reddit_API.Models
     public class Topic
     {
         public int TopicId { get; set; }
-        [Required(ErrorMessage = "Please enter a topic name.")]
         public string Name { get; set; }
     }
 }
